fe46c9134c41c50f0f819b5e92bbfed9d381cca5
bbaktaeho <lth9036@gmail.com>
2021-01-24 16:46:40 +0900

    Add: 타임라인 아이템 뷰 구현

---
+++ app/src/main/java/com/example/noogabab/presentation/ui/timeline/TimeLineActivity.kt
@@ -0,0 +1,12 @@
+package com.example.noogabab.presentation.ui.timeline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.noogabab.R
+
+class TimeLineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_line)
+    }
+}
