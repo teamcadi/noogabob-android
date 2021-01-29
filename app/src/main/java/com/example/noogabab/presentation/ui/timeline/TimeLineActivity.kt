dcfa863d390fe6433d07fc5c97826aacfe112440
bbaktaeho <lth9036@gmail.com>
2021-01-29 14:14:10 +0900

    Add: 타임라인 더미데이터 테스트

--- app/src/main/java/com/example/noogabab/presentation/ui/timeline/TimeLineActivity.kt
+++ app/src/main/java/com/example/noogabab/presentation/ui/timeline/TimeLineActivity.kt
@@ -1,12 +1,41 @@
 package com.example.noogabab.presentation.ui.timeline
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noogabab.R
+import com.example.noogabab.presentation.entity.PresenterTimeLine
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_time_line.*
 
+@AndroidEntryPoint
 class TimeLineActivity : AppCompatActivity() {
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_line)
+
+        val list = ArrayList<PresenterTimeLine>()
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_alarm_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_bab_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_bab_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_bab_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_bab_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_snack_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_bab_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_bab_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_bab_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_alarm_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_alarm_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_alarm_timeline)!!, "밥", "곧 밥을 먹는다"))
+        list.add(PresenterTimeLine("17:30", getDrawable(R.drawable.ic_alarm_timeline)!!, "밥", "곧 밥을 먹는다"))
+
+
+        val adapter = TimeLineAdapter(list)
+        recycler_timeline.layoutManager = LinearLayoutManager(this)
+        recycler_timeline.adapter = adapter
     }
+
 }
