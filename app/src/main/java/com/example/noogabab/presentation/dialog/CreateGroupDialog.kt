cbe5ae7be53305f083cd2041ff84950a91607f6e
bbaktaeho <lth9036@gmail.com>
2021-02-08 16:50:08 +0900

    Save

--- app/src/main/java/com/example/noogabab/presentation/dialog/CreateGroupDialog.kt
+++ app/src/main/java/com/example/noogabab/presentation/dialog/CreateGroupDialog.kt
@@ -5,13 +5,16 @@ import android.app.Dialog
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.util.Log
+import android.view.View
 import android.view.Window
 import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import com.example.noogabab.R
+import com.example.noogabab.presentation.ui.start.createGroup.CreateGroupActivity
 import kotlinx.android.synthetic.main.fragment_dialog.*
 
-class CreateGroupDialog constructor(context: Context): Dialog(context) {
+class CreateGroupDialog constructor(context: Context): Dialog(context), View.OnClickListener {
     init {
         setCanceledOnTouchOutside(false)
         setCancelable(false)
@@ -22,23 +25,17 @@ class CreateGroupDialog constructor(context: Context): Dialog(context) {
         window!!.attributes.width = (size.widthPixels * 0.7).toInt()
         window!!.attributes.height = (size.heightPixels * 0.441).toInt()
         window!!.setBackgroundDrawable(ResourcesCompat.getDrawable(context.resources, R.drawable.dialog_background, null))
-        closeClick()
-        cloneClick()
+
+        btn_dialog_close.setOnClickListener(this)
+        btn_dialog_clone.setOnClickListener(this)
     }
 
-    private fun closeClick() {
-        btn_dialog_close.setOnClickListener {
-            dismiss()
-        }
-    }
-
-    private fun cloneClick() {
-        btn_dialog_clone.setOnClickListener {
-            val text = txt_dialog_key.text
-            val clipboardManager: ClipboardManager = context.getSystemService(Activity.CLIPBOARD_SERVICE) as ClipboardManager
-            val clipData: ClipData = ClipData.newPlainText("create_key", text)
-            clipboardManager.setPrimaryClip(clipData)
-            Toast.makeText(context, "$text 복사 완료", Toast.LENGTH_SHORT).show()
-        }
+    override fun onClick(p0: View?) {
+        val text = txt_dialog_key.text
+        val clipboardManager: ClipboardManager = context.getSystemService(Activity.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipData: ClipData = ClipData.newPlainText("create_key", text)
+        clipboardManager.setPrimaryClip(clipData)
+        Toast.makeText(context, "$text 복사 완료", Toast.LENGTH_SHORT).show()
+        dismiss()
     }
 }
--- app/src/main/java/com/example/noogabab/presentation/ui/start/createGroup/CreateGroupViewModel.kt
+++ app/src/main/java/com/example/noogabab/presentation/ui/start/createGroup/CreateGroupViewModel.kt
@@ -1,6 +1,58 @@
 package com.example.noogabab.presentation.ui.start.createGroup
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.noogabab.presentation.entity.PresenterBobTime
 
-class CreateGroupViewModel: ViewModel() {
+class CreateGroupViewModel : ViewModel() {
+    // value
+    private val _currentDogName = MutableLiveData<String>()
+    private val _currentDogAge = MutableLiveData<Int>()
+    private val _currentDogKind = MutableLiveData<String>()
+    private val _currentBobTimes = MutableLiveData<ArrayList<PresenterBobTime>>()
+
+    // validation
+    private val _currentBtnState = MutableLiveData<Boolean>()
+
+    init {
+        _currentDogName.value = ""
+        _currentDogAge.value = 0
+        _currentDogKind.value = ""
+        _currentBobTimes.value = ArrayList<PresenterBobTime>()
+        _currentBtnState.value = false
+    }
+
+    // public
+    val currentBtnState: LiveData<Boolean>
+        get() = _currentBtnState
+
+    fun updateDogName(input: String) {
+        _currentDogName.value = input
+        isValidation()
+    }
+
+    fun updateDogAge(input: String) {
+        var myValue = 0
+        if (input != "") myValue = input.toInt()
+        _currentDogAge.value = myValue
+        isValidation()
+    }
+
+    fun updateDogKind(input: String) {
+        _currentDogKind.value = input
+        isValidation()
+    }
+
+    fun updateBobTimes(input: PresenterBobTime) {
+        _currentBobTimes.value?.add(input)
+        isValidation()
+    }
+
+    private fun isValidation() {
+        _currentBtnState.value =
+            !(_currentDogName.value == "" || _currentDogAge.value == 0 ||
+                    _currentDogKind.value == "" || _currentBobTimes.value?.size == 0)
+    }
 }
